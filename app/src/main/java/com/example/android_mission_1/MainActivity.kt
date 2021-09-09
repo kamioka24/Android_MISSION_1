36a9582e87dcb2977d8656a8ff5a187e9c02a04b
kamioka24 <keee2404@gmail.com>
2021-09-09 17:55:23 +0900

    [Add]

--- app/src/main/java/com/example/android_mission_1/MainActivity.kt
+++ app/src/main/java/com/example/android_mission_1/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.android_mission_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_mission_1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(this.root)
+        }
     }
 }
